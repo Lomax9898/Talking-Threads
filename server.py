9a18ae7a20dfdb307659492b6bf4f16cf6d10f0b
Rural Young <59895230+RuralYoung@users.noreply.github.com>
2021-02-04 15:55:07 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,65 @@
-# Rural Young N01132160
+# Rural Young n01132160
 # Kevin Lomax n01120755
 
 import socket
+import subprocess
 
-#Initialization
+
+# defining functions used
+def bindPort(socket, port):
+    try:
+        socket.bind(('', port))
+        print("Socket was successfully bound to " + str(port))
+    except Exception as e:
+        port = int(input("Error! Invalid or busy port! Please input a valid port to test on: "))
+        bindPort(s, port)
+
+# Essentially a switch statement
+def switch(case):
+    if case == '1':
+        test = subprocess.check_output("date", shell=True)
+        c.send(str(test).encode())
+    elif case == '2':
+        test = subprocess.check_output('uptime', shell=True)
+        c.send(str(test).encode())
+    elif case == '3':
+        test = subprocess.check_output('free', shell=True)
+        c.send(str(test).encode())
+    elif case == '4':
+        test = subprocess.check_output('netstat', shell=True)
+        c.send(str(test).encode())
+    elif case == '5':
+        test = subprocess.check_output('users', shell=True)
+        c.send(str(test).encode())
+    elif case == '6':
+        test = subprocess.check_output('ps -U root -u root -N', shell=True)
+        c.send(str(test).encode())
+
+# Initialization
 s = socket.socket()
 print("Socket initialized")
 
-port = int(input("Please input the port you are testing on: "))
+# Binding the port
+inputPort = int(input("Please input the port you are testing on: "))
+bindPort(s, inputPort)
 
-try:
-    s.bind(('', port))
-    print("Socket was successfully bound to " + str(port))
-except Exception as e:
-    print(e)
-
+# Listening in for connections
 s.listen(5)
 print("Socket is listening")
 
+# When receiving incoming connections
 while True:
     c, addr = s.accept()
     print("incoming connection from " + str(addr))
+    choice = c.recv(1024)
 
-    msg = 'Connection was successful!'
-    c.send(msg.encode())
+    # Essentially a switch statement that is receiving the choices
+    while choice:
+        try:
+            switch(choice)
+            choice = c.recv(1024)
 
-    c.close()
+        # In case something happens with the client
+        except:
+            print("Was expecting a response, but lost connection")
+            choice = c.recv(1024)
