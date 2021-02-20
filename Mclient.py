93a235e0eb2c16621f887d7c7d910885e60c19f9
Lomax9898 <70455636+Lomax9898@users.noreply.github.com>
2021-02-19 19:30:33 -0500

    Update Mclient.py

--- Mclient.py
+++ Mclient.py
@@ -10,7 +10,7 @@ Restart = True
 while True:
     try:
         port = int(input("Please input the port you are testing on: "))
-        if int(port) > 1025 and int(port) <4998 :
+        if int(port) > 1025 and int(port) < 4998:
             break
         elif port == "exit":
             exit()
@@ -20,7 +20,7 @@ while True:
         print("Provide an integer value...")
         continue
 
-#Inputting the net address
+# Inputting the net address
 netaddress = input("Please input the network address you are testing on: ")
 while Restart == True:
     print("\nOperations:")
@@ -33,11 +33,11 @@ while Restart == True:
         "5 - Current Users, list of users current running on the server \n 6 - Running Processes, list of programs "
         "running on the server")
 
-    #Asking which operation to perform
+    # Asking which operation to perform
     while True:
         try:
             op = input("Please enter a number of the operation you want to perform: ")
-            if int(op)>0 and int(op)<7:
+            if int(op) > 0 and int(op) < 7:
                 break
             elif op == "exit":
                 exit()
@@ -47,7 +47,7 @@ while Restart == True:
             print("Provide an integer value...")
             continue
 
-    #Asking how many times you want that operation to perform
+    # Asking how many times you want that operation to perform
     while True:
         try:
             clients = int(input("How many clients do you want to generate? "))
@@ -60,7 +60,8 @@ while Restart == True:
         except ValueError:
             print("Provide an integer value...")
             continue
-            
+
+
     # This is the instructions for each thread to carry out
     def Cread(times_list):
         c = socket.socket()
@@ -69,9 +70,11 @@ while Restart == True:
         c.send(bytes(op, "utf-8"))
         serverdata = c.recv(1024)
         time_end = time.perf_counter()
+        print('______________________________________________________________________________________________')
         print('Received from the server :', serverdata.decode("ascii"))
         elapsed_time = time_end - time_start
         times_list.append(elapsed_time)
+        print("Client", threading.currentThread().getName(), "took", str(round(elapsed_time, 4)), "seconds")
         c.close()
 
 
@@ -88,20 +91,20 @@ while Restart == True:
     while len(thread_times) != clients:
         time.sleep(1)
 
-    #Further time calculations for each thread
+    # Further time calculations for each thread
     total_time = sum(float(i) for i in thread_times)
     avg_time = total_time / clients
 
-    #Time calculates are printed
+    # Time calculates are printed
     print("The Total Turn-around Time:", str(round(total_time, 4)), "seconds")
     print("The Average Turn-around Time:", str(round(avg_time, 4)), "seconds")
 
-    #Confirmation on seeing the turnaround times for each thread
+    # Confirmation on seeing the turnaround times for each thread
     view_times = input("Would you like to view turn-around Time (elapsed time) for each client request?(yes/no) ")
     if view_times == "yes":
         print(thread_times)
 
-    #Asking if you would like to restart
+    # Asking if you would like to restart
     restart = input("Would you like to do more operations?(yes/no) ")
     if restart == "no":
         restart = False
